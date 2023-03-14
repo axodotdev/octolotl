43ad6649f31d1e0241657e2f292594d01b4beed8
Sara Vieira <hey@iamsaravieira.com>
2023-03-14 18:45:17 +0100

    add suporet for the download endpoint

--- src/index.js
+++ src/index.js
@@ -1,24 +1,34 @@
 export default {
   async fetch(request, env) {
+    const GH_API_URL = "https://api.github.com";
+    const GH_URL = "https://github.com";
     try {
       const { pathname } = new URL(request.url);
       let segments = pathname.split("/");
       if (segments[1] == "releases") {
-          let org = segments[2];
-          let repo = segments[3];
-          let github_url = "https://api.github.com/repos/" + org + "/" + repo + "/releases";
-          let headers = new Headers({
-              "User-Agent"   : "axodotdev-cf-worker-releases"
-          });
-          let gh_request = new Request(github_url, { headers: headers });
+        let endpoint = segments[2];
+        let headers = new Headers({
+          "User-Agent": "axodotdev-cf-worker-releases",
+        });
+        let github_url;
 
-          let gh_response = fetch(gh_request);
-          return gh_response;
-       } else {
-           return new Response("Unsupported", { status: 400 });
+        if (endpoint === "download") {
+          let [_, __, ___, org, repo, version, file] = segments;
+          github_url = `${GH_URL}/${org}/${repo}/releases/download/${version}/${file}`;
+        } else {
+          let [_, __, org, repo] = segments;
+          github_url = `${GH_API_URL}/repos/${org}/${repo}/releases`;
         }
-    } catch(e) {
+
+        let gh_request = new Request(github_url, { headers: headers });
+
+        let gh_response = fetch(gh_request);
+        return gh_response;
+      } else {
+        return new Response("Unsupported", { status: 400 });
+      }
+    } catch (e) {
       return new Response(e.stack, { status: 500 });
     }
-  }
-}
+  },
+};
