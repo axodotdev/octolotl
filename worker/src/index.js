781d05d7a97856e7cee41da7dea80132d15504bc
Ashley Williams <ashley666ashley@gmail.com>
2023-07-24 12:31:13 -0500

    feat(user): add /user endpoint

--- worker/src/index.js
+++ worker/src/index.js
@@ -4,7 +4,11 @@ export default {
       const { pathname } = new URL(request.url);
       let segments = pathname.split("/");
       console.log(segments);
-      if (segments[1] == "releases" && segments.length == 4) {
+      if (segments[1] == "user" && segments.length == 2) {
+          let gh_request = user(request.headers)
+          let gh_response = fetch(gh_request);
+          return gh_response;
+      } else if (segments[1] == "releases" && segments.length == 4) {
           let gh_request = releases(segments[2], segments[3])
           let gh_response = fetch(gh_request);
           return gh_response;
@@ -35,6 +39,7 @@ function index(request) {
 
     releases                GET /releases/{owner}/{repo_name}
     release downloads       GET /dowloads/{owner}/{repo_name}/{tag}/{filename}
+    user                    GET /user
 
 
     >o_o< >o_o< >o_o< >o_o< >o_o< >o_o< >o_o< >o_o< >o_o< >o_o< >o_o< >o_o< >o_o< >o_o<
@@ -45,6 +50,17 @@ function index(request) {
     * received an unhandlable request:
 
     ${request.method} ${request.url}`
+}
+
+function user(original_headers) {
+    let github_url = "https://api.github.com/user";
+    let headers = new Headers({
+        "User-Agent" : "axodotdev-octolotl",
+        "Authorization" : original_headers.get("Authorization"),
+        "Accept": "application/vnd.github+json",
+        "X-GitHub-Api-Version" : "2022-11-28"
+    });
+    return new Request(github_url, { headers: headers });
 }
 
 function releases(owner, repo) {
